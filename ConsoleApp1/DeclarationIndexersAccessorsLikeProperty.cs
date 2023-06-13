3259ca5382a183779aa00ec31ac387f96d7fef69
Le-Holding <68299863+Le-Holding@users.noreply.github.com>
2023-06-13 12:44:10 +0200

    Add project files.

---
+++ ConsoleApp1/DeclarationIndexersAccessorsLikeProperty.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+  public class DeclarationIndexersAccessorsLikeProperty
+  {
+    public string[] TheMusicApp()
+    {
+      var genres = new MusicGenres(); 
+      int count = 0;
+
+      var arr = new string[] { "Bluse", "Rock", "Hip Hop", "Contry", "Soul" };
+      while (count < 5)
+      {
+        //genres[count] = Console.ReadLine();
+        genres[count] = arr[count];
+        count++;
+      }
+
+      string[] arrResult = new string[5];
+      for (int i = 0; i < 5; i++)
+      {
+        arrResult[i] = "Following: " + genres[i];
+      }
+      return arrResult;
+    }
+  }
+
+  internal class MusicGenres
+  {
+    private string[] genres = new string[5];
+
+    //declare an indexer
+
+    // Your code starts here
+   
+  }
+}
---
+++ ConsoleAppTest/DeclarationIndexersAccessorsLikePropertyTests.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleApp1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ConsoleAppTest
+{
+  [TestClass]
+  public class DeclarationIndexersAccessorsLikePropertyTests
+  {
+    [TestMethod]
+    public void TheMusicAppTest()
+    {
+      // Arrange
+      // - nothing to arrange
+
+      // Act
+      var result = new DeclarationIndexersAccessorsLikeProperty().TheMusicApp();
+
+      // Assert
+      Assert.AreEqual(result[0], "Following: Bluse");
+      Assert.AreEqual(result[1], "Following: Rock");
+      Assert.AreEqual(result[2], "Following: Hip Hop");
+      Assert.AreEqual(result[3], "Following: Contry");
+      Assert.AreEqual(result[4], "Following: Soul");
+    }
+  }
+}
